759d9429ca450d5a2f4448eb4bc207000b9f921d
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-12 18:15:03 +0900

    spotテーブルのカラム追加

--- db/migrate/20190911065904_create_spots.rb
+++ db/migrate/20190911065904_create_spots.rb
@@ -1,7 +1,10 @@
 class CreateSpots < ActiveRecord::Migration[6.0]
   def change
     create_table :spots do |t|
-
+      t.string "latitude"
+      t.string "longitude"
+      t.string "image"
+      t.text "comment"
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_065432) do
+ActiveRecord::Schema.define(version: 2019_09_11_065904) do
+
+  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "latitude"
+    t.string "longitude"
+    t.string "image"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
