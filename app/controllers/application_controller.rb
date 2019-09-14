836ccb5eb2cac4c7b2218af1e6a50fd7d6bb128b
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-14 15:38:31 +0900

    サインアウトのリダイレクトの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
