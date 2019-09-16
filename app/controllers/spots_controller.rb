830571302dd766d5dbf838d25a54bc3333ac37b2
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-16 15:15:36 +0900

    application.html.hamlの編集

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,16 +1,22 @@
 class SpotsController < ApplicationController
+  before_action :set_map, only: [:show, :edit, :update, :destroy]
   def index
+    spots =Spot.all
+    @hash = Gmaps4rails.build_markers(@maps) do |map, marker|
+    marker.lat map.latitude
+    marker.lng map.longitude
+    marker.infowindow map.name
   end
 
-  def new
-  end
+  # def new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
-  def search
+  # def search
   end
 end
